0034d186e30bcc219ae6c2a325252158a3b3a3a5
Jae Won Bae <49405805+JaewonB37@users.noreply.github.com>
2023-12-13 00:19:18 +0900

    Update mr_combine.py

--- pre+postprocess/mr_combine.py
+++ pre+postprocess/mr_combine.py
@@ -1,21 +1,43 @@
+from pydub import AudioSegment
+import os
+
+def combine_tracks(vocals_path, accompaniment_path, output_path):
+    vocals = AudioSegment.from_file(vocals_path)
+    accompaniment = AudioSegment.from_file(accompaniment_path)
+
+    # 보컬과 반주의 볼륨 차이 계산
+    diff = accompaniment.dBFS - vocals.dBFS
+
+    # 보컬 볼륨 조절
+    adjusted_vocals = vocals + diff
+
+    # 조절된 볼륨으로 트랙 결합
+    combined = adjusted_vocals.overlay(accompaniment)
+
+    combined.export(output_path, format='wav')
+
+def find_first_wav(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith('.wav'):
+            return os.path.join(directory, filename)
+    return None
+
 def Mr_Combine():
-    from pydub import AudioSegment
-    
-    def combine_tracks(vocals_path, accompaniment_path, output_path):
-        # 보컬과 MR 트랙 로드
-        vocals = AudioSegment.from_file(vocals_path)
-        accompaniment = AudioSegment.from_file(accompaniment_path)
-    
-        # 오디오 트랙 합치기
-        combined = vocals.overlay(accompaniment)
-    
-        # 결과 저장
-        combined.export(output_path, format='wav')
-    
-    # 파일 경로 설정
-    vocals_path = 'path/to/vocals.wav'
-    accompaniment_path = 'path/to/accompaniment.wav'
-    output_path = 'path/to/output/combined.wav'
-    
-    # 트랙 합치기
+    vocals_directory = './audio/pitch_change/vocal'
+    accompaniment_directory = './audio/pitch_change/mr'
+    output_directory = './audio/output'
+
+    vocals_path = find_first_wav(vocals_directory)
+    accompaniment_path = find_first_wav(accompaniment_directory)
+
+    if not vocals_path or not accompaniment_path:
+        print("Vocals or accompaniment file not found.")
+        return
+
+    output_path = os.path.join(output_directory, 'combined_track.wav')
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
     combine_tracks(vocals_path, accompaniment_path, output_path)
+
+Mr_Combine()
